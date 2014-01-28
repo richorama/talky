385209e252e6d3f773b35b307b01482d41668b54
richard astbury <richard.astbury@gmail.com>
2014-01-28 09:35:55 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var flite = require('flite');
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+var speech;
+flite(function(err, s){
+  speech = s;
+  if(err){
+    console.log(err);
+  } else {
+    ask();
+  }
+});
+
+function ask(){
+  rl.question("> ", function(text){
+    speech.say(text);
+    setImmediate(ask);
+  });
+
+}
